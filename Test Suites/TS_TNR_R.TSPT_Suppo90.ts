6b9156563dc66eaba3e65b151ef3a81b32b03841
3Rhizoaui <h3hizaoui@gmail.com>
2023-08-29 10:53:38 +0200

    Ajout du Test Suit Collection

---
+++ Test Suites/TS_TNR_R.TSPT_Suppo90.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_TNR_R.TSPT_Suppo90</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6d67ff3f-f928-471e-94c6-5bbaa9d84f0b</testSuiteGuid>
+   <testCaseLink>
+      <guid>7695af30-a236-4eed-872b-2b0bd6a6f3b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TNR R.TSPT Supp90 (1)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
