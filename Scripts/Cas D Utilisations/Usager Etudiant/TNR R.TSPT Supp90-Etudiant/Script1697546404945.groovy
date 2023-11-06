5add017fc6c7d0aae6fa2ee970fd6a617ac194e9
3Rhizoaui <h3hizaoui@gmail.com>
2023-11-06 13:42:49 +0100

    JDD Chrome

--- Scripts/Cas D Utilisations/Usager Etudiant/TNR R.TSPT Supp90-Etudiant/Script1697546404945.groovy
+++ Scripts/Cas D Utilisations/Usager Etudiant/TNR R.TSPT Supp90-Etudiant/Script1697546404945.groovy
@@ -61,6 +61,7 @@ WebUI.callTestCase(findTestCase('Test Reutilisable/DemandeOuRenouvelle_TitreSejo
 'Appel Brique Réutilisable pour Remplir Le Formulaire Confirmation Usager'
 WebUI.callTestCase(findTestCase('Test Reutilisable/DemandeOuRenouvelle_TitreSejour/FormulaireConfirmation'), [NumEtrangerVisa:NumEtrangerVisa, TypeTitreDeSejour: TypeTitreDeSejour], FailureHandling.STOP_ON_FAILURE)}
 
+
 /*
 'Appel Brique Réutilisable Agent Traitement et Recherche de Demande'
 WebUI.callTestCase(findTestCase('Test Cases/Test Reutilisable/AgentTraitementDemande/AgentRechercheDemande'), [:], FailureHandling.STOP_ON_FAILURE)
