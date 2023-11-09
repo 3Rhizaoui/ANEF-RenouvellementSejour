32c916cc31f3cefcf207207e1f976d5dd4b1e530
3Rhizoaui <h3hizaoui@gmail.com>
2023-11-09 19:00:57 +0100

    Test2

--- Scripts/Test Reutilisable/DemandeOuRenouvelle_TitreSejour/FormulaireJustificatifs/Script1695813884754.groovy
+++ Scripts/Test Reutilisable/DemandeOuRenouvelle_TitreSejour/FormulaireJustificatifs/Script1695813884754.groovy
@@ -65,6 +65,7 @@ println "SpecificDownloadDirectory : " +SpecificDownloadDirectory
 /********************************************************************************************************/
 
 if((TypeTitreDeSejour == "RenouvellementDeTitreSejour") && (Titre == 'Etudiant'))  {
+	WebUI.click(findTestObject('Object Repository/Page_Justificatifs_Usager/Btn_ChoisirFichierTest'))
 	//WebUI.uploadFile(findTestObject('Object Repository/Page_Justificatifs_Usager/Btn_ChoisirFichier_TitreSejourValide'),SpecificDownloadDirectory +'EtatCivil.pdf')
 	WebUI.sendKeys(findTestObject('Object Repository/Page_Justificatifs_Usager/Btn_ChoisirFichier_TitreSejourValide'), SpecificDownloadDirectory +'EtatCivil.pdf')
 	WebUI.delay(02)
